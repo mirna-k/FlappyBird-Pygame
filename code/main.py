d02a2267771b06c1e1898804f961cef43813228b
mirna-k <mirna_krpic@hotmail.com>
2022-12-25 00:50:50 +0100

    Add player and player movement

--- code/main.py
+++ code/main.py
@@ -10,6 +10,28 @@ pygame.display.set_caption('Flappy Bird')
 
 clock = pygame.time.Clock()
 
+# COLORS
+light_blue = (173, 216, 230)
+black = (5, 5, 5)
+
+# ELEMENTS
+PLAYER = pygame.Rect(WIDTH/4-25, HEIGHT/2, 50, 50)
+
+def window():
+    SCREEN.fill(light_blue)
+    pygame.draw.rect(SCREEN, black, PLAYER)
+    pygame.display.update()
+
+def player_movement(player, pressed_key):
+    gravity = 6
+    position = pygame.math.Vector2(player.topleft)
+    position.y += gravity
+    player.y = round(position.y)
+
+    if pressed_key[pygame.K_SPACE]:
+        player.y += -35
+
+
 def main():
     run = True 
     while run: #while run == True:
@@ -18,6 +40,11 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+        pressed_key = pygame.key.get_pressed()
+
+        window()
+        player_movement(PLAYER, pressed_key)
+        
     pygame.quit()
 
 if __name__ == "__main__":
