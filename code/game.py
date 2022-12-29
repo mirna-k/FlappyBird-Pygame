e7b749090161f0a2cf81f13f5ceba24c8dc8cdf6
mirna-k <mirna_krpic@hotmail.com>
2022-12-29 20:20:27 +0100

    Add display_score

--- code/game.py
+++ code/game.py
@@ -13,18 +13,30 @@ class Game():
         # sprite groups
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
+        self.pipe_sprites = pygame.sprite.Group()
 
         # sprite setup
         Background(self.all_sprites)
         Ground([self.all_sprites, self.collision_sprites])
         self.bird = Bird(self.all_sprites)
 
-        #timer
+        # timer
         self.obstacle_timer = pygame.USEREVENT + 1
         pygame.time.set_timer(self.obstacle_timer, 1500)
 
+        # text
+        self.font = pygame.font.Font('graphics/font/BD_Cartoon_Shout.ttf', 30)
+        self.score = 0
+        self.passing_pipe = False
+
+    def display_score(self):
+        score_surface = self.font.render(str(int(self.score)), True, 'black')
+        score_rect = score_surface.get_rect(midtop = (WIDTH/2, HEIGHT/30))
+        self.screen.blit(score_surface, score_rect)
+
     def collisions(self):
-        if pygame.sprite.spritecollide(self.bird, self.collision_sprites, False, pygame.sprite.collide_mask):
+        if pygame.sprite.spritecollide(self.bird, self.collision_sprites, False, pygame.sprite.collide_mask)\
+             or self.bird.rect.bottom <= -100:
             print('collision')
             pygame.quit()
             sys.exit()
@@ -37,6 +49,14 @@ class Game():
             dt = time.time() - last_time
             last_time = time.time()
 
+            if len(self.pipe_sprites) > 0:
+                if self.bird.rect.right < self.pipe_sprites.sprites()[0].rect.right and self.passing_pipe == False:
+                    self.passing_pipe = True
+                    
+                if self.passing_pipe == True and self.bird.rect.left > self.pipe_sprites.sprites()[0].rect.right:
+                    self.score += 1 
+                    self.passing_pipe = False
+
             # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -44,8 +64,8 @@ class Game():
                     sys.exit()
                 if event.type == self.obstacle_timer:
                     pipe_height = random.randint(200, 400)
-                    Obsticle("down", pipe_height, [self.all_sprites, self.collision_sprites])
-                    Obsticle("up", pipe_height, [self.all_sprites, self.collision_sprites])
+                    Obsticle("down", pipe_height, [self.all_sprites, self.collision_sprites, self.pipe_sprites])
+                    Obsticle("up", pipe_height, [self.all_sprites, self.collision_sprites, self.pipe_sprites])
 
             if pygame.key.get_pressed()[pygame.K_SPACE]:
                 self.bird.jump()
@@ -54,6 +74,7 @@ class Game():
             self.all_sprites.update(dt)
             self.collisions()
             self.all_sprites.draw(self.screen)
+            self.display_score()
 
             pygame.display.update()
             self.clock.tick(FRAMERATE)
