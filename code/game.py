adf9e8164597539239550fdcfc3dc0d02bf37ff3
mirna-k <mirna_krpic@hotmail.com>
2022-12-29 03:11:37 +0100

    Add Obsticle and Ground class

--- code/game.py
+++ code/game.py
@@ -1,6 +1,6 @@
-import pygame, sys, time
+import pygame, sys, time, random
 from settings import *
-from sprites import Background, Ground, Bird
+from sprites import Background, Ground, Bird, Obsticle
 
 class Game():
     def __init__(self) -> None:
@@ -15,7 +15,12 @@ class Game():
 
         # sprite setup
         Background(self.all_sprites)
+        Ground(self.all_sprites)
         self.bird = Bird(self.all_sprites)
+
+        #timer
+        self.obstacle_timer = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.obstacle_timer, 1500)
 
 
     def run(self):
@@ -31,6 +36,10 @@ class Game():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == self.obstacle_timer:
+                    pipe_height = random.randint(200, 400)
+                    Obsticle("down", pipe_height, self.all_sprites)
+                    Obsticle("up", pipe_height, self.all_sprites)
 
             if pygame.key.get_pressed()[pygame.K_SPACE]:
                 self.bird.jump()
